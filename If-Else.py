5b8b1b3f84555b8e7b40baf1eac4ec82769895a1
CodeMango75 <141171256+CodeMango75@users.noreply.github.com>
2023-08-23 22:37:08 +0500

    Create If-Else.py
    Basic Codes with IF-Else integration.

---
+++ If-Else.py
@@ -0,0 +1,11 @@
+number = int(input("Enter any Number: "))
+if number % 2 == 0:
+  print("Yes,", number, "Is divisible by 2")
+else:
+  print("No,", number, "Is Not divisible by 2") 
+
+number2 = int(input("Enter Any Number: "))
+if number2 % 2 !=0:
+  print(f'{number2} is not divisible by 2')
+else:
+  print(f'{number2} is divisible by 2')    
